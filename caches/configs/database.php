5c40afb74ab34191abd76583345cdb20fb4c505e
wanghao <wanghao@chinahrt.com>
2018-06-15 09:51:18 +0800

    lasted

--- caches/configs/database.php
+++ caches/configs/database.php
@@ -1,20 +1,4 @@
 <?php
-
-// return array (
-// 	'default' => array (
-// 		'hostname' => '47.104.77.121',
-// 		'port' => 3306,
-// 		'database' => 'cp',
-// 		'username' => 'root',
-// 		'password' => 'w!s@s#db',
-// 		'tablepre' => 'cp_',
-// 		'charset' => 'utf8',
-// 		'type' => 'mysqli',
-// 		'debug' => true,
-// 		'pconnect' => 0,
-// 		'autoconnect' => 0
-// 		),
-// );
 return array (
 	'default' => array (
 		'hostname' => 'localhost',
